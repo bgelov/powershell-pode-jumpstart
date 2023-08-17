f03bae128dc1445cec1ea47ff0b334324f850188
bgelov <belov@ymail.com>
2023-08-16 22:08:03 -0300

    Init repo

---
+++ demo-restart-services/server.ps1
@@ -0,0 +1,60 @@
+﻿Start-PodeServer {
+
+    # Ограничиваем список служб
+    $services = "AdobeARMservice", "bits", "spooler", "wuauserv"
+
+    # Запускаем сервер на http://localhost:8080
+    Add-PodeEndpoint -Address localhost -Port 8080 -Protocol Http
+
+    # Включаем возможность рендеринга и использования .pode файлов
+    Set-PodeViewEngine -Type Pode
+
+    # Маршрут для главной страницы с таблицей
+    Add-PodeRoute -Method Get -Path '/restart-services' -ScriptBlock {
+
+        # Получаем информацию о службах
+        $data = Get-Service $($using:services)
+
+        # Указываем, что мы обращаемся к шаблону restart-services.pode
+        # в директории /views и передаём в шаблон хэш-таблицу
+        # с результатом выполнения Get-Service
+        Write-PodeViewResponse -Path 'restart-services' -Data @{ "services" = $data }
+    }
+
+    # Маршрут для запуска службы
+    Add-PodeRoute -Method Get -Path '/start' -ScriptBlock {
+        
+        # Получаем значение параметра name
+        # В коде шаблона это ?name=$($service.name)
+        # При переходе в браузере, например, http://localhost:8080/start?name=spooler
+        $service_name = $WebEvent.Query["name"]
+
+        # Проверяем, есть ли служба в списке
+        if ($($using:services).Contains($service_name)) {
+
+            # Если есть, выполняем старт службы
+            Start-Service -name $service_name
+        }
+
+        # Перенаправляем пользователя на страницу с таблицей
+        Move-PodeResponseUrl -Url '/restart-services'
+    }
+
+    # Маршрут для остановки службы
+    Add-PodeRoute -Method Get -Path '/stop' -ScriptBlock {
+        $service_name = $WebEvent.Query.name
+        if ($($using:services).Contains($service_name)) {
+            Stop-Service -name $service_name
+        }
+        Move-PodeResponseUrl -Url '/restart-services'
+    }
+
+    # Маршрут для перезапуска службы
+    Add-PodeRoute -Method Get -Path '/restart' -ScriptBlock {
+        $service_name = $WebEvent.Query.name
+        if ($($using:services).Contains($service_name)) {
+            Restart-Service -name $service_name
+        }
+        Move-PodeResponseUrl -Url '/restart-services'
+    }
+}
